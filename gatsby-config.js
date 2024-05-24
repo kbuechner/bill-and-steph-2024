01483710a2314f2926df4aadc28592957e8f56bd
Katherine Buechner <kbuechner@gmail.com>
2024-05-23 20:46:32 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Bill and Steph 2024`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
